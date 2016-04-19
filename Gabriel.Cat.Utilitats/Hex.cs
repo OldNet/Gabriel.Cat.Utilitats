203b53e8c86c72cc3baed929f005c44281a9997f
tetradog <tetradog@gmail.com>
2016-04-19 20:12:29 +0200

    añadida propiedad ByteString 0xNumeroHex

--- Gabriel.Cat.Utilitats/Hex.cs
+++ Gabriel.Cat.Utilitats/Hex.cs
@@ -12,38 +12,38 @@ using System.Collections.Generic;
 
 namespace Gabriel.Cat
 {
-	/// <summary>
-	/// Description of Hex.Por acabar...se tienen que hacer las operaciones...son muy chungas...
-	/// de momento se limita a convertir y operar...en el futuro no se convierte asi pueden ser numero gigantes
-	/// </summary>
-	public struct Hex : IEquatable<Hex>
-	{
-       static LlistaOrdenada<char, char> diccionarioCaracteresValidos;
-      public static readonly string[] caracteresHex = new string[]{
-				"0",
-				"1",
-				"2",
-				"3",
-				"4",
-				"5",
-				"6",
-				"7",
-				"8",
-				"9",
-				"a",
-				"b",
-				"c",
-				"d",
-				"e",
-				"f",
-				"A",
-				"B",
-				"C",
-				"D",
-				"E",
-				"F"
-			};
-		string numberHex;
+    /// <summary>
+    /// Description of Hex.Por acabar...se tienen que hacer las operaciones...son muy chungas...
+    /// de momento se limita a convertir y operar...en el futuro no se convierte asi pueden ser numero gigantes
+    /// </summary>
+    public struct Hex : IEquatable<Hex>
+    {
+        static LlistaOrdenada<char, char> diccionarioCaracteresValidos;
+        public static readonly string[] caracteresHex = new string[]{
+                "0",
+                "1",
+                "2",
+                "3",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+                "9",
+                "a",
+                "b",
+                "c",
+                "d",
+                "e",
+                "f",
+                "A",
+                "B",
+                "C",
+                "D",
+                "E",
+                "F"
+            };
+        string numberHex;
         static Hex()
         {
             diccionarioCaracteresValidos = new LlistaOrdenada<char, char>();
@@ -53,12 +53,12 @@ namespace Gabriel.Cat
 
 
         public Hex(string numero)
-		{
-			if (!ValidaString(numero))
-				throw new Exception("la string contiene caracteres no HEX");
-			this.numberHex = numero;
-		}
-		// this is just an example member, replace it with your own struct members!
+        {
+            if (!ValidaString(numero))
+                throw new Exception("la string contiene caracteres no HEX");
+            this.numberHex = numero;
+        }
+        // this is just an example member, replace it with your own struct members!
         public string Number
         {
             get
@@ -70,191 +70,203 @@ namespace Gabriel.Cat
                 numberHex = value;
             }
         }
-		#region Equals and GetHashCode implementation
-		// The code in this region is useful if you want to use this structure in collections.
-		// If you don't need it, you can just remove the region and the ": IEquatable<Hex>" declaration.
-		
-		public override bool Equals(object obj)
-		{
-			if (obj is Hex)
-				return Equals((Hex)obj); // use Equals method below
-			else
-				return false;
-		}
-		
-		public bool Equals(Hex other)
-		{
-			// add comparisions for all members here
-			return this.numberHex == other.numberHex;
-		}
-		public override string ToString()
-		{
-			return numberHex;
-		}
-		
-		public override int GetHashCode()
-		{
-			// combine the hash codes of all members here (e.g. with XOR operator ^)
-			return numberHex.GetHashCode();
-		}
-		
-		public static bool operator ==(Hex left, Hex right)
-		{
-			return left.Equals(right);
-		}
-		
-		public static bool operator !=(Hex left, Hex right)
-		{
-			return !left.Equals(right);
-		}
-		public static Hex operator +(Hex left, Hex right)
-		{
-			long result = left;
-			result += (long)right;
-			return result;
-		}
-		public static Hex operator -(Hex left, Hex right)
-		{
-			long result = left;
-			result -= (long)right;
-			return result;
-		}
-		public static Hex operator ++(Hex number)
-		{
-			long result = number + 1;
-			
-			return result;
-		}
-		char SigienteCaracter(char caracter)
-		{
-			char caracterNext = '0';
-			switch (caracter) {
-				case 'a':
-				case 'b':
-				case 'c':
-				case 'd':
-				case 'e':
-					caracterNext = (char)(((int)caracter) + 1);
-					break;
-				case 'f':
-					caracterNext = '0';
-					break;
-					
-				case '0':
-				case '1':
-				case '2':
-				case '3':
-				case '4':
-				case '5':
-				case '6':
-				case '7':
-				case '8':
-					caracterNext = (Convert.ToInt32(caracter) + 1).ToString()[0];
-					break;
-				case '9':
-					caracterNext = 'a';
-					break;
-					
-			}
-			return caracterNext;
-		}
-		public static Hex operator --(Hex number)
-		{
-			long result = number - 1;
-			return result;
-		}
-		char AnteriorCaracter(char caracter)
-		{
-			char caracterNext = '0';
-			switch (caracter) {
-					
-				case 'b':
-				case 'c':
-				case 'd':
-				case 'e':
-				case 'f':
-					caracterNext = (char)(((int)caracter) - 1);
-					break;
-				case 'a':
-					caracterNext = '9';
-					break;
-					
-					
-				case '1':
-				case '2':
-				case '3':
-				case '4':
-				case '5':
-				case '6':
-				case '7':
-				case '8':
-				case '9':
-					caracterNext = (Convert.ToInt32(caracter) - 1).ToString()[0];
-					break;
-				case '0':
-					caracterNext = 'f';
-					break;
-					
-			}
-			return caracterNext;
-		}
-		public static Hex operator *(Hex left, Hex right)
-		{
-			long result = left;
-			result *=(long) right;
-			return result;
-		}
-		public static Hex operator /(Hex left, Hex right)
-		{
-			long result = left;
-			result /= (long)right;
-			return result;
-		}
-		#endregion
-		public static implicit operator string(Hex numero)
-		{
-			return numero.numberHex;
-		}
-		public static implicit operator Hex(string numero)
-		{
-			return new Hex(numero);
-		}
-		public static implicit operator Hex(int numero)
-		{
-			return new Hex(QuitaCerosInutiles(numero.ToString("X2")));
-		}
-		public static implicit operator Hex(long numero)
-		{
-			return new Hex(QuitaCerosInutiles(numero.ToString("X4")));
-		}
+        /// <summary>
+        /// Concatena 0x al numero
+        /// </summary>
+        public string ByteString
+        {
+            get
+            {
+                return "0x" + Number;
+            }
+        }
+        #region Equals and GetHashCode implementation
+        // The code in this region is useful if you want to use this structure in collections.
+        // If you don't need it, you can just remove the region and the ": IEquatable<Hex>" declaration.
 
-		static string QuitaCerosInutiles(string numero)
-		{
-			text num=numero;
-			while(num.Count>0&&num[0]=='0')num.Remove(0,1);
-			if(num.Count==0)
-				num="0";
-			return num;
-		}
+        public override bool Equals(object obj)
+        {
+            if (obj is Hex)
+                return Equals((Hex)obj); // use Equals method below
+            else
+                return false;
+        }
 
-		public static implicit operator int(Hex numero)
-		{
-			return Convert.ToInt32((string)numero.numberHex, 16);
-		}
-		public static implicit operator long(Hex numero)
-		{
-			return Convert.ToInt64((string)numero.numberHex, 16);
-		}
-		public static bool ValidaString(string stringHex)
-		{
-			bool valida = true;
+        public bool Equals(Hex other)
+        {
+            // add comparisions for all members here
+            return this.numberHex == other.numberHex;
+        }
+        public override string ToString()
+        {
+            return numberHex;
+        }
+
+        public override int GetHashCode()
+        {
+            // combine the hash codes of all members here (e.g. with XOR operator ^)
+            return numberHex.GetHashCode();
+        }
+
+        public static bool operator ==(Hex left, Hex right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Hex left, Hex right)
+        {
+            return !left.Equals(right);
+        }
+        public static Hex operator +(Hex left, Hex right)
+        {
+            long result = left;
+            result += (long)right;
+            return result;
+        }
+        public static Hex operator -(Hex left, Hex right)
+        {
+            long result = left;
+            result -= (long)right;
+            return result;
+        }
+        public static Hex operator ++(Hex number)
+        {
+            long result = number + 1;
+
+            return result;
+        }
+        char SigienteCaracter(char caracter)
+        {
+            char caracterNext = '0';
+            switch (caracter)
+            {
+                case 'a':
+                case 'b':
+                case 'c':
+                case 'd':
+                case 'e':
+                    caracterNext = (char)(((int)caracter) + 1);
+                    break;
+                case 'f':
+                    caracterNext = '0';
+                    break;
+
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                    caracterNext = (Convert.ToInt32(caracter) + 1).ToString()[0];
+                    break;
+                case '9':
+                    caracterNext = 'a';
+                    break;
+
+            }
+            return caracterNext;
+        }
+        public static Hex operator --(Hex number)
+        {
+            long result = number - 1;
+            return result;
+        }
+        char AnteriorCaracter(char caracter)
+        {
+            char caracterNext = '0';
+            switch (caracter)
+            {
+
+                case 'b':
+                case 'c':
+                case 'd':
+                case 'e':
+                case 'f':
+                    caracterNext = (char)(((int)caracter) - 1);
+                    break;
+                case 'a':
+                    caracterNext = '9';
+                    break;
+
+
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    caracterNext = (Convert.ToInt32(caracter) - 1).ToString()[0];
+                    break;
+                case '0':
+                    caracterNext = 'f';
+                    break;
+
+            }
+            return caracterNext;
+        }
+        public static Hex operator *(Hex left, Hex right)
+        {
+            long result = left;
+            result *= (long)right;
+            return result;
+        }
+        public static Hex operator /(Hex left, Hex right)
+        {
+            long result = left;
+            result /= (long)right;
+            return result;
+        }
+        #endregion
+        public static implicit operator string(Hex numero)
+        {
+            return numero.numberHex;
+        }
+        public static implicit operator Hex(string numero)
+        {
+            return new Hex(numero);
+        }
+        public static implicit operator Hex(int numero)
+        {
+            return new Hex(QuitaCerosInutiles(numero.ToString("X2")));
+        }
+        public static implicit operator Hex(long numero)
+        {
+            return new Hex(QuitaCerosInutiles(numero.ToString("X4")));
+        }
+
+        static string QuitaCerosInutiles(string numero)
+        {
+            text num = numero;
+            while (num.Count > 0 && num[0] == '0') num.Remove(0, 1);
+            if (num.Count == 0)
+                num = "0";
+            return num;
+        }
+
+        public static implicit operator int(Hex numero)
+        {
+            return Convert.ToInt32((string)numero.numberHex, 16);
+        }
+        public static implicit operator long(Hex numero)
+        {
+            return Convert.ToInt64((string)numero.numberHex, 16);
+        }
+        public static bool ValidaString(string stringHex)
+        {
+            bool valida = true;
             stringHex.WhileEach((caracterAComprovar) =>
             {
                 valida = diccionarioCaracteresValidos.Existeix(caracterAComprovar);
                 return valida;
             });
-			return valida;
-			
-		}
-	}
+            return valida;
+
+        }
+    }
 }
